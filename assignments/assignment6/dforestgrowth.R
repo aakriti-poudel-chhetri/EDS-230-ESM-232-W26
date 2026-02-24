7b187f4e3a5654f8769e6a70927b42ead0ece43a
aakikip <poudelaakriti@gmail.com>
2026-02-24 15:10:46 -0800

    Update week 8 lecture

---
+++ assignments/assignment6/dforestgrowth.R
@@ -0,0 +1,22 @@
+#' Forest growth model as a differential equation
+#' @param Time time since start
+#' @param C forest size in units of carbon (kgC)
+#' @param parms - list with four values:
+#' @param r exponential growth rate (before canopy closure)
+#' @param g linear growth rate (after canopy closure)
+#' @param K carrying capacity (kgC)
+#' @param closure canopy closure threshold (kgC)
+#' @return derivative of forest size (C) with time
+
+dforestgrowth <- function(Time, C, parms) {
+  
+  if (C < parms$closure) {
+    # exponential growth (below canopy closure threshold)
+    dC <- parms$r * C
+  } else {
+    # linear growth toward carrying capacity (at or above canopy closure threshold)
+    dC <- parms$g * (1 - C / parms$K)
+  }
+  
+  return(list(dC))
+}
